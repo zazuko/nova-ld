cfd6353c9afb7e7b2618875d1d5b632e5e0b5a88
Adrian Gschwend <ktk@netlabs.org>
2018-11-11 16:02:23 +0700

    INSERT for hops

--- scripts/tdb-query.sh
+++ scripts/tdb-query.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # some INSERTs
-tdbupdate --loc=target/tdb --update=construct/something.rq
-
+tdbupdate --loc=target/tdb --update=sparql/hop.rq
 
 # and after that cleanup
-tdbupdate --loc=target/tdb --update=construct/delete-something.rq
+#tdbupdate --loc=target/tdb --update=construct/delete-something.rq
