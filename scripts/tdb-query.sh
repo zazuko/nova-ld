85ca8664d0b85a2c1fec790a322035bb58ca540f
Adrian Gschwend <ktk@netlabs.org>
2018-11-11 16:07:31 +0700

    Initial triples for getting the distance/rate between two stops

--- scripts/tdb-query.sh
+++ scripts/tdb-query.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # some INSERTs
 tdbupdate --loc=target/tdb --update=sparql/hop.rq
+tdbupdate --loc=target/tdb --update=sparql/rate.rq
 
 # and after that cleanup
 #tdbupdate --loc=target/tdb --update=construct/delete-something.rq
